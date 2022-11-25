2f7a0686fd5f762e390e5bfeca3a3d57561c3a10
Russell Dunphy <russell@accurx.com>
2022-11-25 16:12:40 +0000

    Prompt improvements

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,13 +1,22 @@
-set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
-
-set -l normal_color (set_color normal)
-set -l red_color (set_color red)
-
 function fish_prompt
   set -l current_dir (pwd | sed -e "s|$HOME|~|")
   set -l repo_name
   set -l branch_name
+  set -l branch_ahead
+  set -l branch_behind
+  set -l branch_diverged
 
+  if string match -q -- "~/Code/*" $current_dir
+    set branch_name (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    set clean (git status --porcelain .)
+
+    if test -z "$clean"
+      set -l git_uno (git status -uno .)
+      set branch_ahead (echo $git_uno | grep ahead)
+      set branch_behind (echo $git_uno | grep behind)
+      set branch_diverged (echo $git_uno | grep diverged)
+    end
+  end
 
   if string match -q -- "~/Code/rosemary/src/Web/*" $current_dir
     set current_dir (echo $current_dir | sed -e 's|~/Code/rosemary/src/Web/accuRx.||')
@@ -25,7 +34,7 @@ function fish_prompt
     set current_dir (echo $current_dir | sed -e "s|^$repo_name/*||")
   end
 
-  set branch_name (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+
 
   if test -n "$repo_name"
     set_color blue
@@ -34,6 +43,19 @@ function fish_prompt
     if test -n "$branch_name"
       set_color green
       echo -n -s "[$branch_name] "
+      if not test -z "$clean"
+        set_color yellow
+        echo -n -s "🟡 "
+        set_color normal
+      else if test $branch_diverged
+        echo -n -s "🔀 👉🏻"
+      else if test $branch_behind
+        echo -n -s "⬇️  👉🏻"
+      else if test $branch_ahead
+        echo -n -s "⬆️  👉🏻"
+      else
+        echo -n -s "✅ 👉🏻"
+      end
       set_color normal
       if not test "$current_dir" = ""
         echo -n -s "$current_dir "
@@ -42,8 +64,6 @@ function fish_prompt
   else
     set_color normal
     echo -n -s "$current_dir "
+    echo -n -s "👉🏻 "
   end
-  set_color green
-  echo -n -s "👉🏻 "
-  set_color normal
 end
