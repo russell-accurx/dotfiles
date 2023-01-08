946a2dd0e4d93fdb1225aecb0d08fa68b45e4436
Russell Dunphy <russell@accurx.com>
2023-01-08 12:35:42 +0000

    Clean up

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -4,7 +4,6 @@ abbr -a bu bundle update
 abbr -a be bundle exec
 abbr -a bi bundle install
 abbr -a gpf git push --force-with-lease
-abbr -a gb git browse
 abbr -a gst git status
 abbr -a gp git push
 abbr -a gl git pull
